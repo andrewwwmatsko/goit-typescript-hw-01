1def833deb106cb811e942aabd7dfb187f6c8f48
andrewwwmatsko <matskoandrew@gmail.com>
2024-08-09 07:21:03 +0300

    improved

--- src/basic/5.ts
+++ src/basic/5.ts
@@ -9,9 +9,5 @@ enum DayOfWeek {
 }
 
 const isWeekend = (day: DayOfWeek): boolean => {
-  if (day === DayOfWeek.Saturday || day === DayOfWeek.Sunday) {
-    return true;
-  } else {
-    return false;
-  }
+  return day === DayOfWeek.Saturday || day === DayOfWeek.Sunday;
 };
